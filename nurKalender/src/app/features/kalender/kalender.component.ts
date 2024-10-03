744c46be5f43b470892a467f72c04e18aa4c799d
Emre YAŞAR <emre.yasar.esc97@gmail.com>
2024-10-03 18:58:08 +0200

    add daysOfWeek

--- nurKalender/src/app/features/kalender/kalender.component.ts
+++ nurKalender/src/app/features/kalender/kalender.component.ts
@@ -1,12 +1,25 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-kalender',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './kalender.component.html',
   styleUrl: './kalender.component.scss'
 })
 export class KalenderComponent {
+
+  daysOfWeek: { id: number; name: string }[] = [
+    { id: 1, name: "SUN" },
+    { id: 2, name: "MON" },
+    { id: 3, name: "TUE" },
+    { id: 4, name: "WED" },
+    { id: 5, name: "THU" },
+    { id: 6, name: "FRI" },
+    { id: 7, name: "SAT" }
+  ];
+
+  constructor() { }
 
 }
