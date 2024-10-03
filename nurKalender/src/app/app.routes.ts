33dec79884fb1603b29d4fc58cefcbdfd18082db
Emre YAŞAR <emre.yasar.esc97@gmail.com>
2024-10-03 16:29:08 +0200

    add kalender component

--- nurKalender/src/app/app.routes.ts
+++ nurKalender/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { KalenderComponent } from './features/kalender/kalender.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: KalenderComponent },
+];
